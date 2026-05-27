010ed2ae507bbfca76eb7ade3c1f206c6d03b100
zndr3 <zandersison18@gmail.com>
2026-05-27 11:31:18 +0800

    ADD: website icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	// import favicon from '$lib/assets/favicon.svg';
+	import zapw from '$lib/assets/zapw.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href={zapw} /></svelte:head>
 {@render children()}
